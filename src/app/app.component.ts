e5ef04d9ae2eb01975066c75c2fb282f6ae8be5c
seema padalkar <padalkarseema@gmail.com>
2020-08-25 12:26:49 +0530

    third

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes likes too' ;
+  title = 'angular-tour-of-heroes likes too plus' ;
 }
